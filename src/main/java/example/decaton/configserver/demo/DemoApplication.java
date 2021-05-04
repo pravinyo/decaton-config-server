fbf25504baaa07025b97acea7a00d5df5c88702b
Pravin Tripathi <pravinyo12@gmail.com>
2021-05-04 23:24:31 +0530

    server setup fixed

--- src/main/java/example/decaton/configserver/demo/DemoApplication.java
+++ src/main/java/example/decaton/configserver/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package example.decaton.configserver.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
